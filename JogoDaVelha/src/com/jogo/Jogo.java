3fc540e9a6862c2a87802c5995b194cb773b5928
Giovane <Giovane@lufolenza.home>
2018-03-28 02:02:08 -0300

    Alguns comentários

--- JogoDaVelha/src/com/jogo/Jogo.java
+++ JogoDaVelha/src/com/jogo/Jogo.java
@@ -7,56 +7,49 @@ public class Jogo {
 	public static void main(String[] args) {
 		String jogo[][] = { { "|-----|", "|-----|", "|-----|" }, 
 							{ "|-----|", "|-----|", "|-----|" },
-							{ "|-----|", "|-----|", "|-----|" } };
+							{ "|-----|", "|-----|", "|-----|" } }; // Cria um vetor multidimensional
 		char a = 'a';
 		String i = "a";
-
-		for (int cont = 0; cont < 500; cont++) {
+		System.out.println("Jogo da velha");
+		for (int cont = 0; cont < 500; cont++) { // Cria um looping 'infinito'
 			if (cont % 2 == 0) {
-				i = "X";
+				i = "X"; // Define se o 'índice' vai ser X ou O para poder dar interatividade
 			} else {
 				i = "O";
 			}
 
 			Scanner teclado = new Scanner(System.in);
-			System.out.println("Jogo da velha");
-			System.out.println("==================");
-			/*
-			 * System.out.println("    1    2    3");
-			 * System.out.println("1 |----|----|----|");
-			 * System.out.println("2 |----|----|----|");
-			 * System.out.println("3 |----|----|----|");
-			 */
+			System.out.println("=====================");
 			for (int cont1 = 0; cont1 < jogo.length; cont1++) {
 				for (int cont2 = 0; cont2 < jogo.length; cont2++) {
-					System.out.print(jogo[cont1][cont2]);
+					System.out.print(jogo[cont1][cont2]); // Desenha o vetor, cada jogada ele computa a mudança e aplica
 				}
-				System.out.println();
+				System.out.println(); // Pula linha para poder dar o efeito de 3x3
 			}
 
-			System.out.println("==================");
+			System.out.println("=====================");
 			System.out.println("Quer jogar " + i + " em qual posição?");
-			String linha = teclado.nextLine();
-			String coluna = teclado.nextLine();
-			int contL = Integer.parseInt(linha);
-			int contC = Integer.parseInt(coluna);
-			for (int l = contL - 1; l < jogo.length; l++) {
-				for (int c = contC - 1; c < jogo.length; c++) {
-					if (jogo[l][c] == "|-----|") {
-						jogo[l][c] = "|  " + i + "  |";
+			String linha = teclado.nextLine();    // Lê a linha da jogada
+			String coluna = teclado.nextLine();   // Lê a coluna da jogada
+			int contL = Integer.parseInt(linha);  // Transforma os números em String para poder aplicar no vetor 
+			int contC = Integer.parseInt(coluna); // Transforma os números em String para poder aplicar no vetor 
+			for (int l = contL - 1; l < jogo.length;) { // Retira 1 do número escolhido 
+				for (int c = contC - 1; c < jogo.length;) {
+					if (jogo[l][c] == "|-----|") { // Se o espaço estiver em branco
+						jogo[l][c] = "|  " + i + "  |"; // O espaço recebe o 'índice' do jogador
 						break;
 					} else {
-						System.out.println("Não é possível jogar " + i + " nessa posição");
+						System.out.println("Não é possível jogar " + i + " nessa posição"); // Caso não esteja, é impossível jogar
 						break;
 					}
 				}
 				break;
 			}
-			if (jogo[1][1] == "   " + "X" + "   " && jogo[1][2] == "   " + "X" + "   "
+			/*if (jogo[1][1] == "   " + "X" + "   " && jogo[1][2] == "   " + "X" + "   "
 					&& jogo[1][3] == "   " + "X" + "   ") {
 				System.out.println("Jogador 1 (X) ganhou o jogo!!!!");
 				System.exit(0);
-			}
+			}*/ 
 		}
 	}
 }
